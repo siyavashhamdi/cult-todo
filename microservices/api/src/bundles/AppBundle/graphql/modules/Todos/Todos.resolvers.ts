488404468d67ab874d714492b270567a1a56c20b
Siyavash Hamdi <siyavashhamdi@gmail.com>
2022-02-09 21:59:51 +0330

    CULT-24a89tq Authorizations are done for User and Todo (for query & mutation)

--- microservices/api/src/bundles/AppBundle/graphql/modules/Todos/Todos.resolvers.ts
+++ microservices/api/src/bundles/AppBundle/graphql/modules/Todos/Todos.resolvers.ts
@@ -1,72 +1,86 @@
 import * as X from "@bluelibs/x-bundle";
-import { IResolverMap } from "@bluelibs/graphql-bundle";
+import { IGraphQLContext, IResolverMap } from "@bluelibs/graphql-bundle";
 import { TodoInsertInput, TodoUpdateInput } from "../../../services/inputs";
 import { TodosCollection } from "../../../collections/Todos/Todos.collection";
-import { UserRole } from "@bundles/AppBundle/collections";
 
-const setFilter = async (_, _args, ctx) => {
-  return {
-    filters: {
-      createdById: ctx.userId,
-    },
-  };
-};
+const querySecure = X.Secure([
+  {
+    match: X.Secure.Match.Roles("ADMIN"),
+  },
+  {
+    match: X.Secure.Match.Roles("END_USER"),
+    run: [
+      X.Secure.ApplyNovaOptions((_, _args, ctx: any, _ast) => {
+        const { userId } = ctx;
+        const options = { filters: { createdById: userId } };
+
+        return options;
+      }),
+    ],
+  },
+]);
+
+const mutationModifySecure = X.Secure([
+  {
+    match: X.Secure.Match.Roles("ADMIN"),
+  },
+  {
+    match: X.Secure.Match.Roles("END_USER"),
+    run: [X.Secure.IsUser(TodosCollection, "createdById", "_id")],
+  },
+]);
+
+const mutationInsertSecure = X.Secure([
+  {
+    match: X.Secure.Match.Roles(["ADMIN", "END_USER"]),
+  },
+]);
 
 export default {
   Query: [
-    [X.CheckPermission(["ADMIN", "END_USER"])],
+    [querySecure],
     {
       TodosFindOne: [X.ToNovaOne(TodosCollection)],
-      TodosFind: [X.ToNova(TodosCollection, setFilter)],
+      TodosFind: [X.ToNova(TodosCollection)],
       TodosCount: [X.ToCollectionCount(TodosCollection)],
     },
   ],
   Mutation: [
-    [X.CheckPermission(["ADMIN", "END_USER"])],
+    [],
     {
       TodosInsertOne: [
+        mutationInsertSecure,
         X.ToModel(TodoInsertInput, { field: "document" }),
         X.Validate({ field: "document" }),
         X.ToDocumentInsert(TodosCollection),
         X.ToNovaByResultID(TodosCollection),
       ],
+
       TodosUpdateOne: [
+        mutationModifySecure,
         X.ToModel(TodoUpdateInput, { field: "document" }),
         X.Validate({ field: "document" }),
         X.CheckDocumentExists(TodosCollection),
-        X.Secure([
-          {
-            match: X.Secure.Match.Roles("ADMIN"),
-          },
-          {
-            match: X.Secure.Match.Roles("END_USER"),
-            // run: [X.Secure.IsUser(TodosCollection, "createdBy", "_id")]
-            // // run: X.Secure.ApplyNovaOptions({
-            // //   filters:{
-            // //     createdBy:
-            // //   }
-            // // })
-            // // run: (_: any, args: any, ctx: IGraphQLContext, ast: any) => {
-
-            // // }
-          },
-        ]),
         X.ToDocumentUpdateByID(TodosCollection, null, ({ document }) => ({
           $set: document,
         })),
         X.ToNovaByResultID(TodosCollection),
       ],
+
       TodosDeleteOne: [
+        mutationModifySecure,
         X.CheckDocumentExists(TodosCollection),
         X.ToDocumentDeleteByID(TodosCollection),
       ],
     },
   ],
+
   Subscription: {
     TodosSubscription: {
       resolve: (payload) => payload,
       subscribe: [X.ToSubscription(TodosCollection)],
     },
+
     TodosSubscriptionCount: {
       resolve: (payload) => payload,
       subscribe: [X.ToSubscriptionCount(TodosCollection)],
